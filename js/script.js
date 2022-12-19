1fa742fdf92d7ad1bb475f631054bd1d8ab6c0f3
abylay ospan <210103075@stu.sdu.edu.kz>
2022-12-19 19:41:35 +0600

    added mobile adaptation, made some changes on main page

--- js/script.js
+++ js/script.js
@@ -81,3 +81,17 @@ function submit(){
         alert('Thanks for sharing your email');
     }
 }
+
+function smooth(){
+    const anchors = document.querySelectorAll('a[href*="#"]')
+    for(let anchor of anchors){
+        anchor.addEventListener("click", function(event){
+            event.preventDefault();
+            const blockID = anchor.getAttribute('href')
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            })
+        })
+    }       
+}
